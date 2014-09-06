faff646000a0d0bbad41375d086006206792e522
nachfuellbar <nachfuellbar@googlemail.com>
2014-09-06 15:14:36 +0200

    Change AppData\Local to AppData\Roaming

--- win32/main.c
+++ win32/main.c
@@ -731,7 +731,7 @@ void* png_to_image(void *data, uint16_t *w, uint16_t *h, uint32_t size)
 
 int datapath(uint8_t *dest)
 {
-    if(SUCCEEDED(SHGetFolderPath(NULL, CSIDL_LOCAL_APPDATA, NULL, 0, (char*)dest))) {
+    if(SUCCEEDED(SHGetFolderPath(NULL, CSIDL_APPDATA, NULL, 0, (char*)dest))) {
         uint8_t *p = dest + strlen((char*)dest);
         strcpy(p, "\\Tox"); p += 4;
         CreateDirectory((char*)dest, NULL);
