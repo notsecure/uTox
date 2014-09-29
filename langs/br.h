b54139b329d0635a50e33b5370951b029f7ec2a2
dthiago <thiagoalves@live.com>
2014-09-29 17:18:49 -0300

    brazilian language
    I translated to Brazilian Portuguese. Maybe, a patriotism, i named brazilian language

---
+++ langs/br.h
@@ -0,0 +1,246 @@
+msgid(LANG_NATIVE_NAME)
+msgstr("Língua Brasileira")
+
+msgid(LANG_ENGLISH_NAME)
+msgstr("Brazilian Language")
+
+msgid(REQ_SENT)
+msgstr("Pedido de Amizade Enviado. Assim que aceito o seu amigo irá aparecer online.")
+
+msgid(REQ_RESOLVE)
+msgstr("Resolução de DNS em andamento...")
+
+msgid(REQ_INVALID_ID)
+msgstr("Erro: ID TOX inválido")
+
+msgid(REQ_EMPTY_ID)
+msgstr("Erro: ID Tox não especificado")
+
+msgid(REQ_LONG_MSG)
+msgstr("Erro: Mensagem muito longa")
+
+msgid(REQ_NO_MSG)
+msgstr("Erro: Sem mensagem")
+
+msgid(REQ_SELF_ID)
+msgstr("Error: Tox ID is self ID")
+
+msgid(REQ_ALREADY_FRIENDS)
+msgstr("Error: Este ID TOX já consta na sua lista")
+
+msgid(REQ_UNKNOWN)
+msgstr("Error: Desconhecido")
+
+msgid(REQ_BAD_CHECKSUM)
+msgstr("Error: ID Tox Inválido (checksum)")
+
+msgid(REQ_BAD_NOSPAM)
+msgstr("Error: Invalid Tox ID (bad nospam value)")
+
+msgid(REQ_NO_MEMORY)
+msgstr("Error: Sem memória")
+
+msgid(TRANSFER_NEW)
+msgstr("Novo Arquivo Transferido")
+
+msgid(TRANSFER_STARTED)
+msgstr("Transferência de Arquivo Iniciada")
+
+msgid(TRANSFER___)
+msgstr("...")
+
+msgid(TRANSFER_PAUSED)
+msgstr("Transferência de Arquivo Pausada")
+
+msgid(TRANSFER_BROKEN)
+msgstr("Arquivo Transferido Corrompido")
+
+msgid(TRANSFER_CANCELLED)
+msgstr("Arquivo Transferido Cancelado")
+
+msgid(TRANSFER_COMPLETE)
+msgstr("Arquivo Transferido Completado")
+
+msgid(CALL_CANCELLED)
+msgstr("Chamada Cancelada")
+
+msgid(CALL_INVITED)
+msgstr("Chamada Enviada)
+
+msgid(CALL_RINGING)
+msgstr("Chamando")
+
+msgid(CALL_STARTED)
+msgstr("Chamada Iniciada")
+
+msgid(ADDFRIENDS)
+msgstr("Amigos(s) adicionado(s)")
+
+msgid(TOXID)
+msgstr("ID Tox")
+
+msgid(MESSAGE)
+msgstr("Mensagem")
+
+msgid(SEARCHFRIENDS)
+msgstr("Buscar Amigo(s)")
+
+msgid(ADD)
+msgstr("Adicionar")
+
+msgid(SWITCHPROFILE)
+msgstr("Mudar Perfil")
+
+msgid(FRIENDREQUEST)
+msgstr("Requisições de Amizades")
+
+msgid(USERSETTINGS)
+msgstr("Configurações de usuários")
+
+msgid(NAME)
+msgstr("Nome")
+
+msgid(STATUSMESSAGE)
+msgstr("Estado da Mensagem")
+
+msgid(PREVIEW)
+msgstr("Previsão")
+
+msgid(DEVICESELECTION)
+msgstr("Disoisitivo Selecionado")
+
+msgid(AUDIOINPUTDEVICE)
+msgstr("DIspositivo de Entrada de Audio")
+
+msgid(AUDIOOUTPUTDEVICE)
+msgstr("Dispositivo de Saída de Audio")
+
+msgid(VIDEOINPUTDEVICE)
+msgstr("DIspositivo de Entrada de Video")
+
+msgid(OTHERSETTINGS)
+msgstr("Outras Configurações")
+
+msgid(DPI)
+msgstr("DPI")
+
+msgid(SAVELOCATION)
+msgstr("Salvar Localização")
+
+msgid(LANGUAGE)
+msgstr("Idiona")
+
+msgid(NETWORK)
+msgstr("Rede")
+
+msgid(IPV6)
+msgstr("IPv6:")
+
+msgid(UDP)
+msgstr("UDP:")
+
+msgid(PROXY)
+msgstr("Proxy (SOCKS 5)")
+
+msgid(WARNING)
+msgstr("Mudando configurações de rede/proxy desconectará temporáriamente da rede Tox")
+
+msgid(LOGGING)
+msgstr("Acessando")
+
+msgid(IS_TYPING)
+msgstr("está digitando...")
+
+msgid(COPY)
+msgstr("Copiar")
+
+msgid(COPYWITHOUTNAMES)
+msgstr("Copiar (sem nomes)")
+
+msgid(CUT)
+msgstr("Cortar")
+
+msgid(PASTE)
+msgstr("Colar")
+
+msgid(DELETE)
+msgstr("Excluir")
+
+msgid(SELECTALL)
+msgstr("Selecionar Tudo")
+
+msgid(REMOVE)
+msgstr("Remover")
+
+msgid(LEAVE)
+msgstr("Sair")
+
+msgid(ACCEPT)
+msgstr("Aceitar")
+
+msgid(IGNORE)
+msgstr("Ignorar")
+
+msgid(CLICKTOSAVE)
+msgstr("Clique para Salvar")
+
+msgid(CLICKTOOPEN)
+msgstr("Clique para Abrir")
+
+msgid(CANCELLED)
+msgstr("Cancelado")
+
+msgid(DPI_TINY)
+msgstr("Fino (50%)")
+
+msgid(DPI_NORMAL)
+msgstr("Normal (100%)")
+
+msgid(DPI_BIG)
+msgstr("Grande (150%)")
+
+msgid(DPI_LARGE)
+msgstr("Largo (200%)")
+
+msgid(DPI_HUGE)
+msgstr("Imenso (250%)")
+
+msgid(CONTACTS_FILTER_ALL)
+msgstr("Tudo")
+
+msgid(CONTACTS_FILTER_ONLINE)
+msgstr("Ligado")
+
+msgid(PROXY_DISABLED)
+msgstr("Desabilitado")
+
+msgid(PROXY_FALLBACK)
+msgstr("Retorno")
+
+msgid(PROXY_ALWAYS_USE)
+msgstr("Sempre usar")
+
+msgid(NO)
+msgstr("Não")
+
+msgid(YES)
+msgstr("Sim")
+
+msgid(VIDEO_IN_NONE)
+msgstr("Nenhum")
+
+msgid(VIDEO_IN_DESKTOP)
+msgstr("Área de tTrabalho")
+
+msgid(DEFAULT_FRIEND_REQUEST_MESSAGE)
+msgstr("Por favor, aceite esta requisição de amizade.")
+
+msgid(CONTACTS_FILTER_EDIT_HINT)
+msgstr("Buscar Amigo(s)")
+
+msgid(PROXY_EDIT_HINT_IP)
+msgstr("IP")
+
+msgid(PROXY_EDIT_HINT_PORT)
+msgstr("Porta")
+
